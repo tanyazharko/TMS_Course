35ba4a2acfcc2793fd3a59606784d1883832427c
Tanya <tanyazharko18.09.2001@gmail.com>
2022-10-09 23:03:22 +0300

    add .gitignore

--- Worker/HomeWork_10/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HomeWork_10.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
